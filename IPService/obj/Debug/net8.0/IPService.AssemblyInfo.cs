3326df3037483515e3c08ef9a7b7fb12de4626b3
anna <tikto000000@gmail.com>
2024-04-13 20:01:18 +0300

    fixed some issues

--- IPService/obj/Debug/net8.0/IPService.AssemblyInfo.cs
+++ IPService/obj/Debug/net8.0/IPService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IPService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf281b0867f3888a325da9b950ce916e48a5a5db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbf9dcf9b8cd1b4d01a9f7ac61591022524b1e55")]
 [assembly: System.Reflection.AssemblyProductAttribute("IPService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IPService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
